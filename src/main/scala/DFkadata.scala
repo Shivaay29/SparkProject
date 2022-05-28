2676d68c817bdc9548a80cca841153b75f424a51
Shivaay29 <rahul.malik.19532gmailcom>
2022-05-28 19:02:25 +0530

    data

--- src/main/scala/DFkadata.scala
+++ src/main/scala/DFkadata.scala
@@ -5,10 +5,10 @@ import org.apache.spark.sql.SparkSession
 
 object DFkadata extends App {
   Logger.getLogger("org").setLevel(Level.ERROR)
-  val spark=SparkSession.builder().master("local[*]").appName("DFkadata ").getOrCreate()
-  val sc=spark.sparkContext
+  val spar=SparkSession.builder().master("local[*]").appName("DFkadata ").getOrCreate()
+  val sc=spar.sparkContext
   sc.setLogLevel("Error")
-  import spark.implicits._
+  import spar.implicits._
   val pehlardd=sc.parallelize(1 to 10).map(x=>(x,"DF ka Data"))
   pehlardd.collect.foreach(println)
   //val df=Seq("Sno.","Simple String")
